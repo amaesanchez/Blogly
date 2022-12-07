4140e63592e7518f1915e4b2c41f940e772a22ea
Arlaine Sanchez <amaesanchez@gmail.com>
2022-12-07 17:31:00 -0500

    add app context

--- models.py
+++ models.py
@@ -1,11 +1,22 @@
+"""Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-"""Models for Blogly."""
-
 def connect_db(app):
     """Connect to database."""
 
+    app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class User(db.Model):
+    """ table for Users """
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True,
+        autoincrement=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=True)
