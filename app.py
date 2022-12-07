ef62be6c09b56f6bba8f02ffa7aeda5e0e0cdbcf
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2022-12-07 18:27:27 -0500

    added some templates and routes

--- app.py
+++ app.py
@@ -16,8 +16,44 @@ connect_db(app)
 db.create_all()
 
 @app.get("/")
+def send_to_users():
+    """ redirect to users """
+    return redirect("/users")
+
+    # users = User.query.all()
+    # return render_template
+
+@app.get("/users")
 def show_users():
-    """ show user list """
+    """displays users"""
 
     users = User.query.all()
-    return render_template
+
+    return render_template(user_listing.html, users=users)
+
+@app.post("/users-new")
+def add_user():
+"""adds a user to the table"""
+
+    user_first_name = request.form.get("first_name")
+    user_last_name = request.form.get("last_name")
+    user_image_url = request.form.get("image_url")
+
+    user = User(first_name=user_first_name, last_name=user_last_name, image_url=user_image_url)
+
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.get("/users-new")
+def display_new_user_form():
+    """displays the new user form"""
+
+    return render_template("user_form.html")
+
+@app.get("/users/<int:user-id>")
+def display_user_details():
+    """displays the users details"""
+
+    return render_template("user_detail.html")
+
