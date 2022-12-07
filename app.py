5690701f47c82e4c1abdcd5f17388bf80648acb3
Arlaine Sanchez <amaesanchez@gmail.com>
2022-12-07 15:46:50 -0500

    add templates and root

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, request, flash, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "abc123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 """Models for Blogly."""
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
