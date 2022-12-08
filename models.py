951fdb8aa8837a1773080372de549590a0986a74
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2022-12-08 14:46:05 -0500

    added the post model

--- models.py
+++ models.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 DEFAULT_IMAGE_URL = 'https://i.pinimg.com/550x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg'
 
+# now = datetime.datetime.utcnow
+
+
 def connect_db(app):
     """Connect to database."""
 
@@ -18,6 +21,28 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True,
         autoincrement=True)
+
     first_name = db.Column(db.Text, nullable=False)
+
     last_name = db.Column(db.Text, nullable=False)
+
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+class Post(db.Model):
+    """ Posts made by users """
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+    primary_key=True,
+    autoincrement=True)
+
+    title = db.Column(db.Text, nullable=False)
+
+    content = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime) #might be db.DateTime(timezone=True) also eventually should have default=now
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='posts')
